f76dc8abb47c16ed764970eb7f89a273159e2561
Alexis4021 <alan.chavezj@uanl.edu.mx>
2023-05-17 21:39:03 -0600

    Responsivo Identidad-virtual

--- identidad-virtual.php
+++ identidad-virtual.php
@@ -24,6 +24,10 @@
 <!DOCTYPE html>
 <html lang="es">
 <link rel="stylesheet" href="assets/css/index.css">
+<link rel="stylesheet" href="plugins/slick/slick.css">
+	<link rel="stylesheet" href="plugins/font-awesome/fontawesome.min.css">
+	<link rel="stylesheet" href="plugins/font-awesome/brands.css">
+	<link rel="stylesheet" href="plugins/font-awesome/solid.css">
   <head>
 
     <meta charset="utf-8">
@@ -111,7 +115,7 @@
           <div class="col-md-12">
             <div class="text-content">
               <h4>NFC</h4>
-              <h2>Tarjeta de Presentacion</h2>
+              <h2>Identidad-Virtual</h2>
             </div>
           </div>
         </div>
@@ -123,25 +127,19 @@
         <div class="row">
           <div class="col-md-4 col-xs-12">
             <div>
-              <img src="assets/images/product-1-370x270.jpg" alt="" class="img-fluid wc-image">
+              <img src="https://maidenware1.s3.us-east-2.amazonaws.com/Tarjeta+1.jpg" alt="" class="img-fluid wc-image">
             </div>
             <br>
             <div class="row">
-              <div class="col-sm-4 col-xs-6">
+              <div class="col-md-4 col-xs-12">
                 <div>
-                  <img src="assets/images/product-1-370x270.jpg" alt="" class="img-fluid">
+                  <img src="https://maidenware1.s3.us-east-2.amazonaws.com/tarjeta+2.jpg" alt="" class="img-fluid">
                 </div>
                 <br>
               </div>
-              <div class="col-sm-4 col-xs-6">
+              <div class="col-md-4 col-xs-12">
                 <div>
-                  <img src="assets/images/product-2-370x270.jpg" alt="" class="img-fluid">
-                </div>
-                <br>
-              </div>
-              <div class="col-sm-4 col-xs-6">
-                <div>
-                  <img src="assets/images/product-3-370x270.jpg" alt="" class="img-fluid">
+                  <img src="https://maidenware1.s3.us-east-2.amazonaws.com/mercado_pago_ofertas.webp" alt="" class="img-fluid">
                 </div>
                 <br>
               </div>
@@ -150,12 +148,12 @@
 
           <div class="col-md-8 col-xs-12">
             <form action="#" method="post" class="form">
-              <h2>Tarjeta de Presentación</h2>
+              <h2>Identidad-Virtual</h2>
 
               <br>
 
               <p class="lead">
-                <small><del> $500.00</del></small><strong class="text-primary">$300.00</strong>
+                <small><del> $500.00</del></small><strong class="text-primary">$400.00</strong>
               </p>
 
               <br>
@@ -257,17 +255,99 @@
       </div>
     </div>
 
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="inner-content">
-              <p>Copyright © 2020 Company Name - Template by: <a href="https://www.phpjabbers.com/">PHPJabbers.com</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+    <footer class="section-sm bg-tertiary">
+	<div class="container">
+		<div class="row justify-content-between">
+			<div class="col-lg-2 col-md-4 col-6 mb-4">
+				<div class="footer-widget">
+					<h5 class="mb-4 text-primary font-secondary">Service</h5>
+					<ul class="list-unstyled">
+						<li class="mb-2"><a href="service-details.html">Personal loans</a>
+						</li>
+						<li class="mb-2"><a href="service-details.html">Home Equity Loans</a>
+						</li>
+						<li class="mb-2"><a href="service-details.html">Student Loans</a>
+						</li>
+						<li class="mb-2"><a href="service-details.html">Mortgage Loans</a>
+						</li>
+						<li class="mb-2"><a href="service-details.html">Payday Loans</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-6 mb-4">
+				<div class="footer-widget">
+					<h5 class="mb-4 text-primary font-secondary">About</h5>
+					<ul class="list-unstyled">
+						<li class="mb-2"><a href="#!">Benefits</a>
+						</li>
+						<li class="mb-2"><a href="#!">Careers</a>
+						</li>
+						<li class="mb-2"><a href="#!">Our Story</a>
+						</li>
+						<li class="mb-2"><a href="#!">Team</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-6 mb-4">
+				<div class="footer-widget">
+					<h5 class="mb-4 text-primary font-secondary">Help</h5>
+					<ul class="list-unstyled">
+						<li class="mb-2"><a href="contact.html">Contact Us</a>
+						</li>
+						<li class="mb-2"><a href="faq.html">FAQs</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-12 newsletter-form">
+				<div style="background-color: #F4F4F4; padding: 35px;">
+					<h5 class="mb-4 text-primary font-secondary">Newsletter</h5>
+					<div class="pe-0 pe-xl-5">
+						<form action="#!" method="post" name="mc-embedded-subscribe-form" target="_blank">
+							<div class="input-group mb-3">
+								<input type="text" class="form-control shadow-none bg-white border-end-0" placeholder="Email address"> <span class="input-group-text border-0 p-0">
+                    <button class="input-group-text border-0 bg-primary" type="submit" name="subscribe"
+                      aria-label="Subscribe for Newsletter"><i class="fas fa-arrow-right"></i></button>
+                  </span>
+							</div>
+							<div style="position: absolute; left: -5000px;" aria-hidden="true">
+								<input type="text" name="b_463ee871f45d2d93748e77cad_a0a2c6d074" tabindex="-1">
+							</div>
+						</form>
+					</div>
+					<p class="mb-0">Suscribete a nuestro newsletter para recibir mas informacion, promociones y novedades sobre nosotros</p>
+				</div>
+			</div>
+		</div>
+		<div class="row align-items-center mt-5 text-center text-md-start">
+			<div class="col-lg-4">
+        <a href="index.html">
+          <img loading="prelaod" decoding="async" class="img-fluid" width="160" src="https://maidenware1.s3.us-east-2.amazonaws.com/MAIDENWARE.png" alt="Wallet">
+        </a>
+			</div>
+			<div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
+				<ul class="list-unstyled list-inline mb-0 text-lg-center">
+					<li class="list-inline-item me-4"><a class="text-black" href="https://drive.google.com/file/d/146sIx1Si3z-YF62OkX-Ww-XVmKRynovZ/view?usp=sharing">Politica de Privacidad</a>
+					</li>
+					<li class="list-inline-item me-4"><a class="text-black" href="terms.html">Terms &amp; Conditions</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-lg-4 col-md-6 text-md-end mt-4 mt-md-0">
+				<ul class="list-unstyled list-inline mb-0 social-icons">
+					<li class="list-inline-item me-3"><a title="Explorer Facebook Profile" class="text-black" href="https://www.facebook.com/profile.php?id=100064601179100"><i class="fab fa-facebook-f"></i></a>
+					</li>
+					<!--<li class="list-inline-item me-3"><a title="Explorer Twitter Profile" class="text-black" href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+					</li>-->
+					<li class="list-inline-item me-3"><a title="Explorer Instagram Profile" class="text-black" href="https://www.instagram.com/maidenware/"><i class="fab fa-instagram"></i></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
 
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
